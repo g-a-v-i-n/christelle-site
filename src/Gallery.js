3dafd2295c07bdb8e8563091726458d733be8240
Gavin Atkinson <gatkinso@risd.edu>
2017-07-26 15:41:14 -0400

    add photo layout positioning

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react'
 
 class Asset extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      homeCoordinates: {x:0, y:0},
+      dimensions: {width:0, height:0},
+      loaded: false,
+      positionLatch: false,
+    }
+  }
+
+  onImgLoad = (e) => {
+    this.setState({
+      loaded: true,
+      dimensions: {
+        width: e.target.offsetWidth,
+        height: e.target.offsetHeight,
+      },
+    })
+  }
+
   render() {
+    const index = this.props.index
+    if (this.state.loaded && !this.state.positionLatch) {
+      this.props.addToGalleryList({index, dimensions: this.state.dimensions})
+    }
     return (
-      <div className={'image_outerContainer'}>
-        <img alt={''} src={this.props.src} />
-      </div>
+      <img onLoad={(e) => this.onImgLoad(e)} alt={''} src={this.props.thumbURL} />
     )
   }
 }
 
 class Gallery extends Component {
-
   render() {
+    let translateStyle = {}
+    if (this.props.homeCoordinates !== null) {
+      translateStyle = { transform: `translate(${this.props.homeCoordinates.x}px, ${this.props.homeCoordinates.y}px)`}
+    }
     return (
-      <div id={'pile'}>
-        <Asset src={this.props.thumbURL} />
+      <div id={'pile'} style={translateStyle}>
+        <Asset
+          addToGalleryList={this.props.addToGalleryList}
+          index={this.props.index}
+          onImgLoad={this.props.onImgLoad}
+          thumbURL={this.props.thumbURL}
+        />
       </div>
     )
   }
--- src/Home.js
+++ src/Home.js
@@ -4,40 +4,59 @@ import { Gallery } from './Gallery'
 class Home extends Component {
   constructor(props) {
     super(props)
-    this.props.getGalleryContent()
     this.state = {
-      dimensions: {},
+      dimensionArr: [],
+      allLoaded: false,
+      homeCoordinates: [],
     }
-    this.onImgLoad = this.onImgLoad.bind(this)
-    }
-
-    onImgLoad({target:img}) {
-      this.setState({
-        dimensions:{
-          height:img.offsetHeight,
-          width:img.offsetWidth,
-        },
-      })
-    }
+    this.galleryList = []
+  }
 
   componentWillMount = () => {
+    this.props.getGalleryContent()
+  }
+
+  addToGalleryList = (gallery) => {
+    this.galleryList = this.galleryList.concat(gallery)
+    if (this.galleryList.length === this.props.projects.length) {
+      const homeCoordinates = this.arrangePiles()
+      this.setState({
+        homeCoordinates,
+      })
+    }
   }
 
   generatePiles = () => {
-    return this.props.projects.map((project) => {
-      const origin = [30, 60]
-
+    return this.props.projects.map((project, index) => {
+      let homeCoordinates = null
+      if (this.state.homeCoordinates.length === this.props.projects.length) {
+        homeCoordinates = this.state.homeCoordinates[index]
+      }
       return (
         <Gallery
+          homeCoordinates={homeCoordinates}
+          addToGalleryList={this.addToGalleryList}
           thumbURL={project.fields.assets[0].fields.file.url}
-          assets ={project.fields.assets}
-          key={`Pile_${project.fields.projectName}`}
-          client={project.fields.client}
-          date={project.fields.date}
-          projectName={project.fields.projectName}
-          projectType={project.fields.projectType}
+          fields={project.fields}
+          index={index}
+          key={project.sys.id}
         />
       )
+    })
+  }
+  // REMINDER make offsets in viewWidth /viewHeight
+  arrangePiles = () => {
+    let lastPosX = 0
+    let lastPosY = 0
+    let lastHeight = this.galleryList[0].dimensions.height
+    // REMINDER: resort the dimensionArr by index
+    return this.props.projects.map((project, index) => {
+      lastHeight = this.galleryList[index].dimensions.height
+      const newY = lastPosY + (lastHeight / ((Math.random() * 2) + 1.1)) // randomize gos here
+      lastPosY = newY
+
+
+      return ({x:40, y:newY})
     })
   }
 
