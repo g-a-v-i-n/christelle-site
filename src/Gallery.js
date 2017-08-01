c4f8727468d01168df0c17a048b44f6665fe3287
Gavin Atkinson <gatkinso@risd.edu>
2017-07-31 21:59:06 -0400

    add also for image placement, add overlay

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,42 +1,72 @@
 import React, { Component } from 'react'
+import lodash from 'lodash'
+import classnames from 'classnames'
 
 class Asset extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      homeCoordinates: {x:0, y:0},
-      dimensions: {width:0, height:0},
-      loaded: false,
-    }
+
+  onMouseEnter = (e, index) => {
+    e.stopPropagation()
+    this.props.setScreenState(true, index)
   }
 
-  handleImageLoaded() {
-    this.setState({ loaded: true})
-
-
-  }
-
-  handleImageErrored() {
-    this.setState({ imageStatus: 'failed to load' })
+  onMouseLeave = (e, index) => {
+    e.stopPropagation()
+    this.props.setScreenState(false, index)
   }
 
   render() {
+    let imgClasses = classnames({
+      'imgOn': this.props.hover,
+      'imgFade': !this.props.hover,
+    })
+
     return (
-      <img ref={`imageGallery_${this.props.index}`} src={this.props.thumbURL} onLoad={(e) => this.props.addToGalleryList(e, this.props.index)} />
+      <img
+        className={imgClasses}
+        alt={`imageGallery_${this.props.index}`}
+        src={this.props.thumbURL}
+        onLoad={(e) => this.props.addToGalleryList(e, this.props.index)}
+        onMouseEnter={(e) => this.onMouseEnter(e, this.props.index)}
+        onMouseLeave={(e) => this.onMouseLeave(e, this.props.index)}/>
     )
   }
 }
 
 class Gallery extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+    }
+  }
 
+  handleGalleryClose = () => {
+    this.setState({
+      open: true,
+    })
+  }
+
+  handleGalleryClose = () => {
+    this.setState({
+      open: false,
+    })
+  }
+  render() {
+    let translations = {}
+    if (!lodash.isEmpty(this.props.galleryCoordinates)) {
+      translations = {
+        transform: `translate(${this.props.galleryCoordinates.x}px,${this.props.galleryCoordinates.y}px)`,
+      }
+    }
     return (
-      <div id={'pile'}>
+      <div id={'pile'} style={translations}>
         <Asset
+          hover={this.props.hover}
           addToGalleryList={this.props.addToGalleryList}
           index={this.props.index}
           onImgLoad={this.props.onImgLoad}
           thumbURL={this.props.thumbURL}
+          setScreenState={this.props.setScreenState}
         />
       </div>
     )
--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,53 @@
 import React, { Component } from 'react'
 import { Gallery } from './Gallery'
-import _ from 'lodash'
+import lodash from 'lodash'
+import classnames from 'classnames'
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
       dimensionArr: [],
-      galleryListWithCoords: [],
       loadedGalleries: [],
+      hoverStates: [],
+      arrangeDone: false,
+      minHeight: 0,
+      loaded: false,
     }
   }
 
   addToGalleryList = (gallery, index) => {
-      let imgObject = {
-        width:gallery.target.clientWidth,
-        height: gallery.target.clientHeight,
-        index,
-        x: 0,
-        y: 0,
-      }
-      this.setState({
-        loadedGalleries: this.state.loadedGalleries.concat(gallery.target),
-      }, () => {this.arrangePiles()})
+    let imgObject = {
+      width:gallery.target.clientWidth,
+      height: gallery.target.clientHeight,
+      index,
+      x: 0,
+      y: 0,
     }
-
+    this.setState({
+      loadedGalleries: this.state.loadedGalleries.concat(imgObject),
+      hoverStates: this.state.hoverStates.concat(true),
+    }, () => {this.arrangePiles()})
+  }
 
   generatePiles = () => {
     return this.props.projects.map((project, index) => {
-      let thisGalleryCoords = {}
+      let galleryCoordinates = {}
+      let hover = true
       // only set this gallery coords when there is something to set
-      if (this.state.galleryListWithCoords.length !== 0) {
-        let thisGalleryCoords = this.state.galleryListWithCoords[index]
+      if (this.state.arrangeDone) {
+        galleryCoordinates = this.state.loadedGalleries[index]
+        hover = this.state.hoverStates[index]
       }
       return (
         <Gallery
-          thisGalleryCoords={thisGalleryCoords}
+          hover={hover}
+          galleryCoordinates={galleryCoordinates}
           addToGalleryList={this.addToGalleryList}
           thumbURL={project.fields.assets[0].fields.file.url}
           fields={project.fields}
           index={index}
+          setScreenState={this.setScreenState}
           key={project.sys.id}
         />
       )
@@ -49,33 +57,99 @@ class Home extends Component {
   // REMINDER make offsets in viewWidth /viewHeight
   arrangePiles = () => {
     if (this.props.projects.length === this.state.loadedGalleries.length) {
-      const globalOrigin = 0
+      let lastTop = 0
+      let lastBottom = 0
+      let lastLeft = 0
+      let lastRight = 0
 
-      const initalMaxOverlap = 0.4
-      let thisMaxOverlap = initalMaxOverlap
-      const minOverlap = 0.1
+      let viewWidth = window.innerWidth
+      let windowCenter = viewWidth / 2
 
-      let offsetFromTop = 0
-      let thisY = 0
-      let lastHeight = 0
-      // REMINDER: resort the dimensionArr by index
-      return this.props.projects.map((project, index) => {
-        // maxOverlap = (lastHeight / )
-        let overlap = lastHeight * ( Math.random() * (maxOverlap - minOverlap) + minOverlap)
-        let thisY = lastHeight - overlap + offsetFromTop
-        offsetFromTop = thisY
-        lastHeight = this.state.galleryList[index].dimensions.height
-        return ({x:40, y:thisY, overlap: overlap, bottom: thisY + lastHeight})
+      let deltaY = 0
+      let deltaX = 0
+
+      // resort to correct any load race condition re-ordering
+      let galleryList = lodash.orderBy(this.state.loadedGalleries, ['index'], ['asc'])
+      let arrangedGalleries = this.props.projects.map((project, index) => {
+        //define vars
+        const img = galleryList[index]
+        const thisHeight = img.height
+        const thisWidth = img.width
+        // do the algo for X, Y
+        if (index === 0) {
+          // first one
+          lastRight = img.width
+          lastBottom = img.height
+          deltaX = windowCenter - (img.width - (viewWidth * 0.04))
+        } else if (index <= this.props.projects.length - 1 && index !== 0) {
+          deltaY = lastBottom - (thisHeight * 0.08)
+          lastBottom = img.height + deltaY
+          if (index % 2 === 1) {
+            // right
+            deltaX = windowCenter - (thisWidth * 0.08)
+          } else {
+            // left
+            deltaX = windowCenter - (thisWidth - (viewWidth * 0.04))
+          }
+          // REMINDER: not needed unless there is forward / backward awareness
+        } else {
+          // last one
+          deltaY = lastBottom - (thisHeight * 0.08)
+          lastBottom = img.height + deltaY
+
+          deltaX = lastRight - (thisWidth * 0.08)
+          lastRight = img.width + deltaX
+        }
+
+        // set the results
+        img.y = deltaY
+        img.x = deltaX
+        return img
+      })
+      this.setState({
+        minHeight: lastBottom,
+        arrangeDone: true,
+        loadedGalleries: arrangedGalleries,
       })
     }
+  }
 
+  setScreenState = (isHovering, index) => {
+    // false equals fade, true equals show
+    if (isHovering) {
+      let states = this.state.hoverStates
+      states = states.map((state) => {
+        return false
+      })
+      states[index] = true
+      this.setState({
+        hoverStates: states,
+      })
+    } else {
+      let states = this.state.hoverStates
+      states = states.map((state) => {
+        return true
+      })
+      this.setState({
+        hoverStates: states,
+      })
+    }
   }
 
   render() {
+    let minHeight = {
+      minHeight: `${this.state.minHeight}px`,
+    }
 
+    let loadingState = classnames({
+      'loadingOn': !this.state.arrangeDone,
+      'loadingOff': this.state.arrangeDone,
+    })
     return (
       <main className={'home'}>
-        <content>
+      <div id={'loadingScreen'} className={loadingState} />
+        <content style={minHeight}>
+        <div className={'centerline'} />
           {this.generatePiles()}
         </content>
         <footer>{'Christelle De Castro'}</footer>
